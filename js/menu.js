188a12272643e5803f693cc4d2846ba3ac6c2320
fungaviotas <32000846+fungaviotas@users.noreply.github.com>
2018-06-20 19:43:42 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+$(document).ready(main); var contador = 1; 
+
+function main(){ 
+
+	$('.menu_bar').click(function(){ 
+		if(contador == 1){ 
+			contador = 0; 
+			$('nav').animate({left: '0'}); 
+			$('body').addClass('noscroll'); 
+		} else{ 
+			contador = 1; 
+			$('nav').animate({left: '-100%'}); 
+			$('body').removeClass('noscroll'); 
+		} 
+	}); 
+	$(window).resize(function(){ 
+		contador = 1; 
+		if(window.innerWidth > 800) { 
+			$('nav').removeAttr('style'); 
+			$('body').removeClass('noscroll'); }
+    }); 
+
+}
